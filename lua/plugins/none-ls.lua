36f61114e27d99c59c8d5a6c464193ed75edf58f
Daniel Ajibewa <ajibewadannyboi@gmail.com>
2025-02-22 02:27:10 +0100

    Try to confige prettier

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -1,16 +1,15 @@
 return {
-  "nvimtools/none-ls.nvim",
-  config = function()
-    local null_ls = require("null-ls")
-    null_ls.setup({
-      sources = {
-        null_ls.builtins.formatting.stylua,
-        null_ls.builtins.formatting.prettier,
-        null_ls.builtins.diagnostics.eslint_d,
-        null_ls.builtins.formatting.isort,
-        null_ls.builtins.formatting.black
-      },
-    })
-
-  end,
+    "nvimtools/none-ls.nvim",
+    config = function()
+        local null_ls = require "none-ls"
+        null_ls.setup {
+            sources = {
+                null_ls.builtins.formatting.stylua,
+                null_ls.builtins.formatting.prettier,
+                null_ls.builtins.diagnostics.eslint_d,
+                null_ls.builtins.formatting.isort,
+                null_ls.builtins.formatting.black,
+            },
+        }
+    end,
 }
